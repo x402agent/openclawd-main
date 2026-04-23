af8370da5fe9a41c47a802049b9c89820b4148e7
8bitsats <gimmetheschrute@gmail.com>
2026-04-23 16:42:06 -0400

    ffff

---
+++ clawd-code-cli/src/tools/index.ts
@@ -0,0 +1,7 @@
+export { BashTool } from "./bash.js";
+export { TextEditorTool } from "./text-editor.js";
+export { MorphEditorTool } from "./morph-editor.js";
+export { TodoTool } from "./todo-tool.js";
+export { ConfirmationTool } from "./confirmation-tool.js";
+export { SearchTool } from "./search.js";
+export { SolanaTool } from "./solana.js";
